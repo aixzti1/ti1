c5602f6de6bcd510da83de88bee3b0c24da071b7
aixzti1 <elpedromihael@gmail.com>
2025-01-06 22:42:39 +0300

    Resized test trainer LSTM Model

--- tests/test_trainers.py
+++ tests/test_trainers.py
@@ -6,8 +6,8 @@ from src.utils.config import Config
 def test_trainer_lstm():
     config = Config()
     model = LSTMModel()
-    x = torch.randn(10, config.seq_len, config.input_size)
-    y = torch.randn(10)
+    x = torch.randn(50, config.seq_len, config.input_size)
+    y = torch.randn(50)
     dataset = list(zip(x, y))
     loader = torch.utils.data.DataLoader(dataset, batch_size=config.batch_size)
     trainer = TrainerLSTM(model, loader, config)
