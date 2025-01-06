6229569f292d27e5cdb3e4253cec1159b20f2599
aixzti1 <elpedromihael@gmail.com>
2025-01-06 21:54:43 +0300

    Refractored logger structure

--- utils/logger.py
+++ utils/logger.py
@@ -2,8 +2,8 @@ import datetime
 
 class Logger:
     def __init__(self):
-        self.log_file = 'log.txt'
+        self.log_file = 'log_1.txt'
     def log(self, message):
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        with open(self.log_file, 'a') as f:
+        with open(self.log_file, 'a+') as f:
             f.write(f'{now} - {message}\n')
