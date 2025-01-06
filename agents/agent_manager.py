866dc7772ec1c3f67681a5da3d4a4a1911e56bc9
aixzti1 <elpedromihael@gmail.com>
2025-01-07 00:05:00 +0300

    Improved agent manager stability

--- agents/agent_manager.py
+++ agents/agent_manager.py
@@ -4,6 +4,15 @@ import pickle
 class AgentManager:
     def __init__(self, save_path='agents/'):
         self.save_path = save_path
+
+    def delete_agent(self, name):
+        path = os.path.join(self.save_path, f'{name}.pkl')
+        if os.path.exists(path):
+            os.remove(path)
+    def list_agents(self):
+        if not os.path.exists(self.save_path):
+            return []
+        return [f.split('.pkl')[0] for f in os.listdir(self.save_path) if f.endswith('.pkl')]
     def save_agent(self, agent):
         if not os.path.exists(self.save_path):
             os.makedirs(self.save_path)
@@ -12,11 +21,3 @@ class AgentManager:
     def load_agent(self, name):
         with open(os.path.join(self.save_path, f'{name}.pkl'), 'rb') as f:
             return pickle.load(f)
-    def list_agents(self):
-        if not os.path.exists(self.save_path):
-            return []
-        return [f.split('.pkl')[0] for f in os.listdir(self.save_path) if f.endswith('.pkl')]
-    def delete_agent(self, name):
-        path = os.path.join(self.save_path, f'{name}.pkl')
-        if os.path.exists(path):
-            os.remove(path)
